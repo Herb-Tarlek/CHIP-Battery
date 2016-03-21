dac51bb18b66478c55645c35c394595eb0badfde
Herb-Tarlek <nmfree@CHIP-2.attlocal.net>
2016-03-21 06:20:28 +0000

    A little better script, coming along...

--- battery3.sh
+++ battery3.sh
@@ -2,30 +2,66 @@
 
 ###########################################################
 #
-#Battery Stats for the C.H.I.P.
-# ***Original by CapnBry***
-#
-# ***v.2 by Herb_Tarlek***
+#       :   ``   :
+#       ;   ;;   ;
+#       ;   ;;   ;
+#    ;;;;;;;;;;;;;;;;     Battery Stats for the C.H.I.P.
+#    ;.````````````.;     ***Original by CapnBry***
+#    ;`            `;
+# :;;;`            `;;;:  ***v.2 by Herb_Tarlek***
+#    ;`  ;;;       `;
+#    ;`  ;;;.      `;
+#    ;`  ;;;,      `;     Special thanks to zerotri on IRC
+# `;;;`   .`;;;;;;;;;;;`  Come join us on IRC! #chipsters on
+# `;;;`       `,:;;;;;;`  Freenode.
+#    ;`            `;
+#    ;`            `;
+#    ;`            `;
+# :;;;`            `;;;:
+#    ;`            `;
+#    ;.````````````.;
+#    ;;;;;;;;;;;;;;;;
+#       ;   ;;   ;
+#       ;   ;;   ;
+#       :   ``   :
 #
 ###########################################################
 
-BATTERY_FILE="batterystats3.txt"
+STATS_FILE="batterystats.$(date +"%R").txt"
+MYSELF=$(whoami)
+BOLD_TEXT=$(tput bold)
+NORMAL_TEXT=$(tput sgr0)
 
-# THIS PART IS NOT FINISHED YET
-# check for i2c group
+
+# Check for i2c group
+#
+
+if [ $(id -Gn "$MYSELF" | grep -c "i2c") -eq "0" ]; then
+	echo "I'm sorry, $MYSELF, you're not in the ${BOLD_TEXT}i2c${NORMAL_TEXT} group."
+	echo "Please execute '${BOLD_TEXT}sudo usermod -a -G i2c $MYSELF${NORMAL_TEXT}', logout and login (or reboot),"\
+	"and try again."
+	exit
+fi
 #
 #
 #
-#if (id -Gn | grep -c "i2c") > "0"; then
-#	MYSELF=$(whoami)
-#	echo "You are not a member of the i2c group.\
-#	Please execute 'sudo usermod -a -G i2c $MYSELF', logout and back in or reboot,\
-#	and try again."
-#	exit
-#fi
-#
-#
-#
+
+echo "This script will test your battery in both charging and discharging mode."
+echo "The data will be collected in a CSV (Comma Seperated Value) file named ${BOLD_TEXT}$STATS_FILE${NORMAL_TEXT}."
+echo "If this filename is OK, press enter; otherwise, give a new filename and press enter."
+echo -n "New filename [Enter for default]:"
+read NEW_FILENAME
+if [ -n "$NEW_FILENAME" ]; then
+	STATS_FILE=$NEW_FILENAME
+fi
+
+#Write column headers to data file
+echo "Time, mV, mA" >> $STATS_FILE
+
+
+#Prettier output header
+printf "==================================\n"
+printf "Press Ctrl-C to end the script.\n"
 
 # set ADC enabled for all channels
 ADC=$(/usr/sbin/i2cget -y -f 0 0x34 0x82)
@@ -65,8 +101,13 @@ while [ true ] ; do
     CURRENT_TIME=$(date +"%H:%M:%S")
     BATTERY_CSV="$CURRENT_TIME,$BAT_VOLT,$BAT_ICHG"
 
-    echo $X
-    echo $BATTERY_CSV >> $BATTERY_FILE
+#write the data to the CSV file
+    echo $BATTERY_CSV >> $STATS_FILE
+
+#Make the output refresh on the same line.
+# \r is carriage return
+# \e[K is the escape code to clear the line
+    printf "\r\e[K%smV, %smA" $BAT_VOLT $BAT_ICHG
 
 # wait 5 seconds
     sleep 5
